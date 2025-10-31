d1849dcb094e69d73fd96777f20d0b824700c5c3
Duy Hoàng <duypo03cbvn@gmail.com>
2025-10-31 14:24:54 +0700

    fix unmatching keyword

--- src/utils/common.py
+++ src/utils/common.py
@@ -94,7 +94,7 @@ def get_model(model_name,
             num_channels=num_channels,
             im_size=img_size,
             hidden_size=hidden_size,
-            num_classes=num_classes
+            output_size=num_classes
         )
 
     return model
